c55ba653cc424f3938abcbcf3ff67aa2fa979b67
ckchivatsi <kelvinchivatsi@hotmail.com>
2017-03-27 17:45:32 +0300

    Made a simple form that will enable one to vote

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -14,7 +15,10 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
@@ -22,4 +26,16 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+        'error_message': "You didn't select a choice"
+    }
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=question_id))
